6186ac0fdb978de2c2b3953d3ded63ab95a0cf0e
Anne-Marie Esteves <annemariee@free.fr>
2015-03-07 13:58:13 +0100

    add module node-twitter to app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var Twitter = require('node-twitter');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -10,4 +11,24 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
+var twitterRestClient = new Twitter.RestClient(
+    'CONSUMER_KEY',
+    'CONSUMER_SECRET',
+    'TOKEN',
+    'TOKEN_SECRET'
+);
+
+twitterRestClient.statusesHomeTimeline({}, function(error, result) {
+    if (error)
+    {
+        console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+    }
+
+    if (result)
+    {
+        console.log(result);
+    }
 });
